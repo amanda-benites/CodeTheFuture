01e594b041b4190ae29b4cd29e3ed040952fab72
Amanda Benites <amandabenites2005@gmail.com>
2022-04-28 17:02:26 -0300

    Organizei as funcionalidades em js

--- projeto/src/app/codigo.js
+++ projeto/src/app/codigo.js
@@ -13,54 +13,66 @@ function btnclassif() {
 function lista() {
 
     const varrerCasa = {
-    nome: "Varrer a casa"
-    , tempo: 20
+        nome: "Varrer a casa"
+        , tempo: 20
     }
+    document.getElementById("varrerCasa").innerText = nome
 
     const aspirarCasa = {
         nome: "Aspirar a Casa"
         , tempo: 15
         }
+    document.getElementById("aspirarCasa").innerText = tempo
 
     const passarPano = {
         nome: "Passar o pano no chão"
         , tempo: 20
         }
+    document.getElementById("passarPano").innerText = tempo
 
     const lavarLouca = {
-        nome: "Varrer a casa"
+        nome: "Lavar a louça"
         , tempo: 20
         }
+    document.getElementById("lavarLouca").innerText = tempo
 
     const tirarPo = {
-        nome: "Varrer a casa"
+        nome: "Tirar o pó"
         , tempo: 20
         }
+    document.getElementById("tirarPo").innerText = tempo
 
     const arrumarQuarto = {
-        nome: "Varrer a casa"
+        nome: "Arrumar o quarto"
         , tempo: 20
         }
+    document.getElementById("arrumarQuarto").innerText = tempo
 
     const arrumarSala = {
-        nome: "Varrer a casa"
+        nome: "Arrumar a sala"
         , tempo: 20
         }
+    document.getElementById("arrumarSala").innerText = tempo
 
     const arrumarCozinha = {
-        nome: "Varrer a casa"
+        nome: "Arrumar a Cozinha"
         , tempo: 20
         }
+    document.getElementById("arrumarCozinha").innerText = tempo
 
     const banheiro = {
-        nome: "Varrer a casa"
+        nome: "Arrumar e limpar o banheiro"
         , tempo: 20
         }
+    document.getElementById("banheiro").innerText = tempo
 
     const arrumarRoupeiro = {
-        nome: "Varrer a casa"
+        nome: "Arrumar o roupeiro"
         , tempo: 20
         }
+    document.getElementById("arrumarRoupeiro").innerText = tempo
+    
+    
 }
 
 function selecao() {
