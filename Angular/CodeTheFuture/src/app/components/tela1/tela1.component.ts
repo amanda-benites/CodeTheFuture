f18d56da10016a9090660663ba57e9fff9168a1b
Amanda Benites <amandabenites2005@gmail.com>
2022-05-04 16:47:29 -0300

    tentando avançar nas funcionalidades da checkbox

--- Angular/CodeTheFuture/src/app/components/tela1/tela1.component.ts
+++ Angular/CodeTheFuture/src/app/components/tela1/tela1.component.ts
@@ -7,47 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tela1.component.scss']
 })
 export class Tela1Component implements OnInit {
+    tasks: any;
+    selectAll = false;
 
-  constructor() { }
+  constructor() {
+    this.tasks = [
+    { name: "Varrer a casa", tempo: 20 },
+    { name: "Aspirar a Casa", tempo: 15 },
+    { name: "Passar o pano no chão", tempo: 40 },
+    { name: "Lavar a louça", tempo: 10 },
+    { name: "Tirar o pó", tempo: 20 },
+    { name: "Arrumar o quarto", tempo: 30 },
+    { name: "Arrumar a sala", tempo: 25 },
+    { name: "Arrumar a Cozinha", tempo: 25 },
+    { name: "Arrumar e limpar o banheiro", tempo: 50 },
+    { name: "Arrumar o roupeiro", tempo: 20 }    
+  ] }
 
   ngOnInit(): void {
   }
 
-  soma = 0;
+  teste() {
+    console.log(this.tasks);
+  }
 
-    tasks = [
-      { name: "Varrer a casa", tempo: 20 },
-      { name: "Aspirar a Casa", tempo: 15 },
-      { name: "Passar o pano no chão", tempo: 40 },
-      { name: "Lavar a louça", tempo: 10 },
-      { name: "Tirar o pó", tempo: 20 },
-      { name: "Arrumar o quarto", tempo: 30 },
-      { name: "Arrumar a sala", tempo: 25 },
-      { name: "Arrumar a Cozinha", tempo: 25 },
-      { name: "Arrumar e limpar o banheiro", tempo: 50 },
-      { name: "Arrumar o roupeiro", tempo: 20 }    
-    ]
-
-    check: boolean
-
-
-   onCheck() {
-    if (this.check == true) {
-      
+  updateCheck() {
+    if (this.selectAll === true) {
+      this.tasks.map((task) => {
+        task.checked = true;
+      });
+    } else {
+      this.tasks.map((task) => {
+        task.checked = false;
+      });
     }
-
-
-   }
-
-
-
-
-
-
-
-
-
-  //somaTempo() {
-  //   this.soma += this.tasks[1]
-  // }
+  }
 }
--- Angular/CodeTheFuture/src/app/components/teste/teste.component.ts
+++ Angular/CodeTheFuture/src/app/components/teste/teste.component.ts
@@ -11,8 +11,16 @@ export class TesteComponent {
 
   constructor() {
     this.pizzaIng = [
-      { name: 'Varrer a casa', time: 3, checked: false },
-      { name: 'Lavar a louça', time: 10, checked: true },
+      { name: "Varrer a casa", tempo: 20, checked: false },
+      { name: "Aspirar a Casa", tempo: 15, checked: false },
+      { name: "Passar o pano no chão", tempo: 40, checked: false },
+      { name: "Lavar a louça", tempo: 10, checked: false },
+      { name: "Tirar o pó", tempo: 20, checked: false },
+      { name: "Arrumar o quarto", tempo: 30, checked: false },
+      { name: "Arrumar a sala", tempo: 25, checked: false },
+      { name: "Arrumar a Cozinha", tempo: 25, checked: false },
+      { name: "Arrumar e limpar o banheiro", tempo: 50, checked: false },
+      { name: "Arrumar o roupeiro", tempo: 20, checked: false }
     ];
   }
 
